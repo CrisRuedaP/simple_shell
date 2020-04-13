f10954b0134902fa43dcfde3c1cb296e653afbb3
s-macias <1552@holbertonschool.com>
2020-04-13 19:10:41 +0000

    adding header file

---
+++ header.h
@@ -0,0 +1,15 @@
+#ifndef _HEADER_H
+#define _HEADER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <sys/wait.h>
+#include <strings.h>
+
+extern char **environ;
+
+#endif
