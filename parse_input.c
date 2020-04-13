522fe68ad886fad63ad59d6ae6e4134916917828
CrisRuedaP <crisruedap@gmail.com>
2020-04-13 21:38:53 +0000

    fix betty style

--- parse_input.c
+++ parse_input.c
@@ -9,18 +9,18 @@
 
 int parse_input(char *user_input, char **argv)
 {
-  int ac = 0;
-  char *parameter, exec_name;
-  char *temp = user_input;
+	int ac = 0;
+	char *parameter, exec_name;
+	char *temp = user_input;
 
-  user_input = strtok(user_input, "\n\t\r");
-  argv[ac] = exec_name; /*El comando, el primer argumento del input*/
-  for (ac = 1; exec_name != NULL; ac++)
-  {
-    parameter = strtok(NULL, "\n\t\r");
-    temp = parameter;
-    argv[ac] = parameter;
-  }
-  argv[ac] = NULL;
-  return (ac);
+	user_input = strtok(user_input, "\n\t\r");
+	argv[ac] = exec_name; /*El comando, el primer argumento del input*/
+	for (ac = 1; exec_name != NULL; ac++)
+	{
+		parameter = strtok(NULL, "\n\t\r");
+		temp = parameter;
+		argv[ac] = parameter;
+	}
+	argv[ac] = NULL;
+	return (ac);
 }
--- shell.c
+++ shell.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-        ssize_t read_bytes = 0; /**número de caracteres leidos y el salto de línea*/
-        size_t MAX_BUFFER_SIZE;
-        char *user_input = NULL;
-        pid_t ch_pid = 0;
+	ssize_t read_bytes = 0; /**número de caracteres leídos y el salto de línea*/
+	size_t MAX_BUFFER_SIZE;
+	char *user_input = NULL;
+	pid_t ch_pid = 0;
         char *argv[20];
         int status = 0;
         int input_count = 0;
         int valid_exec = 0;
 
         print_prompt("$ ", 2);
-        read_bytes = getline(&user_input, &MAX_BUFFER_SIZE, STDIN);
+        read_bytes = getline(&user_input, &MAX_BUFFER_SIZE, stdin);
         while (read_bytes != -1)
         {
                 if (*user_input == '\n')
@@ -35,23 +35,21 @@ int main(void)
                                 else if (ch_pid == 0)
                                 {
                                         execve(argv[0], argv, environ);
-                                        exit(exit_status);
+                                        exit(1);
                                 }
-                                else 
+                                else
                                         wait(&status);
                         }
-                        else if  (valid_exec != 0)
-                                print_error(input_count, not found);
-                free(*argv);
+                        else if (valid_exec != 0)
+                                print_error(input_count, not_found);
+                        free(*argv);
                 }
-        user_input = NULL;
-        input_count++;
-        print_prompt("$ ", 2);
-        read_bytes = getline(&user_input, &MAX_BUFFER_SIZE, STDIN);
+                user_input = NULL;
+                input_count++;
+                print_prompt("$ ", 2);
+                read_bytes = getline(&user_input, &MAX_BUFFER_SIZE, stdin);
         }
         _putchar('\n');
         free(user_input);
         return (0);
-        }
 }
- 
